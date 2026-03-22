802b607d0247d30a286fb9777c3f89ca93b706c1
Lund12346 <156418594+Lund12346@users.noreply.github.com>
2026-03-23 04:03:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import requests
 
-# Ye details Streamlit Secrets se aayengi
+# Secrets se details uthana
 APP_ID = st.secrets["APP_ID"]
 APP_SECRET = st.secrets["APP_SECRET"]
-# Jab aap app deploy karenge, wahan ka URL yahan badal dena
-REDIRECT_URI = "https://fbtokengenerator-csjam4sw4kuwjghquihjm4.streamlit.app/"
-
+REDIRECT_URI = "https://fbtokengenerator-csjam4sw4kuwjghquihjm4.streamlit.app/" 
 
 st.title("🚀 FB Token Extractor")
 
+# Login URL
 login_url = f"https://www.facebook.com/v18.0/dialog/oauth?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&scope=public_profile,email"
 
 if st.button("Login with Facebook"):
     st.markdown(f'[Authorize Karne Ke Liye Yahan Click Karein]({login_url})', unsafe_allow_html=True)
 
+# Token nikalne ka logic
 if "code" in st.query_params:
     code = st.query_params["code"]
     url = f"https://graph.facebook.com/v18.0/oauth/access_token?client_id={APP_ID}&redirect_uri={REDIRECT_URI}&client_secret={APP_SECRET}&code={code}"
@@ -24,4 +24,4 @@ if "code" in st.query_params:
         st.success("Mubarak ho! Aapka Token niche hai:")
         st.code(res["access_token"])
     else:
-        st.error("Galti: " + str(res.get('error', {}).get('message')))                                                       
+        st.error("Galti: " + str(res.get('error', {}).get('message')))
